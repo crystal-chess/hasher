a6348c5d30c2a6543b936d4c42d258119e1a3506
ezoheux <46251064+ezoheux@users.noreply.github.com>
2018-12-29 20:51:37 -0500

    Update HasherTest.php

--- tests/HasherTest.php
+++ tests/HasherTest.php
@@ -1,1 +1,32 @@
+<?php
 
+declare(strict_types=1);
+/**
+ * Crystal Chess - Basic password hashing with PHP.
+ *
+ * @author Crystal Chess Contributors <https://github.com/orgs/crystal-chess/people>
+ *
+ * @link <https://github.com/crystal-chess/hasher> Crystal Hasher.
+ */
+
+namespace CrystalChess\Chess;
+
+use PHPUnit\Framework\TestCase;
+use CrystalChess\Hasher;
+use UnexpectedValueException;
+
+/**
+ * Hasher test case.
+ */
+class HasherTest extends TestCase
+{
+    public function testConstructorExceptionFromTheInstanceGetter()
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $options = [
+            'algo' => 'md5' // The old way of hashing. We do not use this anymore.
+        ];
+        $hasher = new Hasher($options);
+        // Should fail due to exception thrown.
+    }
+}
